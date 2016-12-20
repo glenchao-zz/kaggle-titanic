32690d5d127f614e87b62e84426a44cdd38519d8
Glen Chao <glenchao@outlook.com>
2016-12-19 23:32:47 -0500

    Add predict_gender.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # kaggle-titanic
 https://www.kaggle.com/c/titanic
+
+* `predict_gender.py` generates `prediction_gender.csv` by simply predicting all female survived and all male died
+* `predict_numpy.py` uses numpy lib to generate `predction_gender_pclass_fare.csv` by calculating the probably of survival given the `gender`, `pclass`, and `fare` of the passenger.
---
+++ predict_gender.py
@@ -0,0 +1,21 @@
+import csv as csv
+import numpy as np
+
+### OPEN FILES ###
+# open file
+file = open('./test.csv', 'rb')
+file_obj = csv.reader(file)
+# get headers
+header = file_obj.next()
+
+# open pred file to write our prediction
+pred_file = open('./prediction_gender.csv', 'wb')
+pred_file_obj = csv.writer(pred_file)
+# set pred_file header
+pred_file_obj.writerow(["PassengerId", "Survived"])
+
+for row in file_obj:
+    pred_file_obj.writerow([row[0], 1 if row[3] == "female" else 0])
+
+file.close()
+pred_file.close()
